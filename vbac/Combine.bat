a122eaafa111c75b4b245a8ae1b8ae970bf52207
Cygnan <mail@cygnan.com>
2017-06-16 01:05:28 +0900

    Bugs fixed

--- vbac/Combine.bat
+++ vbac/Combine.bat
@@ -1,5 +1,6 @@
 cd %~dp0
 copy /Y ..\code.vb .\src\game-of-life.xlsm\Sheet1.dcm
+copy /Y ..\game-of-life.xlsm .\bin\
 cscript vbac.wsf combine
 move /Y .\bin\game-of-life.xlsm ..\
 del .\src\game-of-life.xlsm\Sheet1.dcm
